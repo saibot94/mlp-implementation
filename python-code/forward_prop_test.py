dac53c89dcb6201cbe61359241b7bce8f603e0ba
cristian.schuszter <cschuszter@metabiota.com>
2017-06-22 17:00:12 +0300

    Added validation of grad. descent

--- python-code/forward_prop_test.py
+++ python-code/forward_prop_test.py
@@ -1,5 +1,6 @@
 from pymlp.neural_network import NeuralNetwork
 import numpy as np
+from numpy.linalg import norm
 
 def test_forward_prop():
     X = np.array(([3, 5], [5, 1], [10, 2]), dtype=float)
@@ -15,5 +16,61 @@ def test_forward_prop():
     print(str(result))
 
 
+def test_error_func():
+    y = np.array(([1,2,3,4]), dtype=float)
+    yhat = np.array(([1,1,2,4]), dtype=float)
+
+    nn = NeuralNetwork(y.shape[0])
+
+
+def test_gradient_desc_numerical():
+    X = np.array(([3, 5], [5, 1], [10, 2]), dtype=float)
+    y = np.array(([75], [82], [93]), dtype=float)
+
+    nn = NeuralNetwork(X.shape[1])
+    def f(x):
+        return x**2
+
+    epsilon = 1e-4
+    x = 1.5
+    numericGradient = compute_numerical_gradients(nn, X, y)
+    nngradients = nn.compute_gradients(X, y)
+
+    print("Grads (normal): ")
+    print(str(nngradients))
+
+    print("Numerical gradients:")
+    print(str(numericGradient))
+
+    assert (norm(nngradients - numericGradient) / norm(nngradients + numericGradient)) < 0.00000001
+
+    
+
+
+
+def compute_numerical_gradients(nn, x, y):
+    params = nn.getParams()
+    nrgrad = np.zeros(params.shape)
+    perturb = np.zeros(params.shape)
+    e = 1e-4
+    for p in range(len(params)):
+        perturb[p] = e
+        nn.setParams(params + perturb)
+        loss2 = nn.cost_function(x, y)
+
+
+        nn.setParams(params - perturb)
+        loss1 = nn.cost_function(x, y)
+        nrgrad[p] = (loss2 - loss1) / (2*e)
+        perturb[p] = 0
+    nn.setParams(params)
+
+    return nrgrad
+
+
 if __name__ == '__main__':
+    print("=========== Forward propagation test")
     test_forward_prop()
+    print("=========== Error (Cost) func test")
+    test_error_func()
+    numerically_test_gradient_desc()
--- python-code/pymlp/neural_network.py
+++ python-code/pymlp/neural_network.py
@@ -5,12 +5,16 @@ def sigmoid_activation(z):
     return 1 / (1 + np.exp(-z))
 
 
+def sigmoid_derivative(z):
+    return np.exp(-z) / ((1 + np.exp(-z))**2)
+
+
 class NeuralNetwork(object):
 
     def __init__(self, feature_no,
                  hidden_layer_neurons=3,
                  output_layer_size=1,
-                 activation_func=sigmoid_activation):
+                 alpha=1):
         """
         Create a neural network with a few params avaiable.
 
@@ -23,22 +27,80 @@ class NeuralNetwork(object):
 
         output_layer_size - number of possible classes (one output in the case of regression)
 
-        activation_func - the function which takes a matrix and activates each of the outputs
+        alpha - the step for the gradient descent algorithm
         """
         self.feature_no = feature_no
         self.hidden_layer_neurons = hidden_layer_neurons
         self.output_layer_size = output_layer_size
-        self.activation_func = activation_func
+        self.alpha = alpha
 
         self.w1 = np.random.randn(self.feature_no, self.hidden_layer_neurons)
-        self.w2 = np.random.randn(self.hidden_layer_neurons, self.output_layer_size)
+        self.w2 = np.random.randn(
+            self.hidden_layer_neurons, self.output_layer_size)
 
     def is_regression_network(self):
         return self.output_layer_size == 1
 
     def forwardPropagation(self, x):
         self.z2 = np.dot(x, self.w1)
-        self.a2 = self.activation_func(self.z2)
+        self.a2 = sigmoid_activation(self.z2)
         self.z3 = np.dot(self.a2, self.w2)
-        response = self.activation_func(self.z3)
+        response = sigmoid_activation(self.z3)
         return response
+
+    def cost_prime_function(self, x, y):
+        self.yhat = self.forwardPropagation(x)
+
+        # error of the first derivation
+        delta3 = np.multiply(-(y - self.yhat), sigmoid_derivative(self.z3))
+        dJdw2 = np.dot(self.a2.T, delta3)
+
+        delta2 = np.dot(delta3, self.w2.T) * sigmoid_derivative(self.z2)
+        dJdw1 = np.dot(x.T, delta2)
+
+        return dJdw1, dJdw2
+
+    def apply_changes(self, dJdw1, dJdw2):
+        self.w1 = self.w1 - self.alpha * dJdw1
+        self.w2 = self.w2 - self.alpha * dJdw2
+
+    def cost_function(self, x, y):
+        """
+        Estimate the error of the classifier by using the sum of squared errors sigma(e^2). This is what we'll have to minimize
+        """
+        self.yhat = self.forwardPropagation(x)
+        return 0.5 * sum((self.yhat - y) ** 2)
+
+    def iterate(self, X, y):
+        dJdw1, dJdw2 = self.cost_prime_function(X, y)
+
+        self.apply_changes(dJdw1, dJdw2)
+
+        print("New error is: ")
+        print(str(self.cost_function(self.yhat, y)))
+        print("Prediction is: ")
+        print(str(self.yhat))
+
+    def compute_gradients(self, x, y):
+        dJdw1, dJdw2 = self.cost_prime_function(x, y)
+        print("dj1: " + str(dJdw1))
+        print("dj2: " + str(dJdw2))
+        return np.concatenate((dJdw1.ravel(), dJdw2.ravel()))
+
+
+    #Helper Functions for interacting with other classes:
+    def getParams(self):
+        #Get W1 and W2 unrolled into vector:
+        params = np.concatenate((self.w1.ravel(), self.w2.ravel()))
+        return params
+
+
+            
+    def setParams(self, params):
+        #Set W1 and W2 using single paramater vector.
+        W1_start = 0
+        W1_end = self.hidden_layer_neurons * self.feature_no
+        self.w1 = np.reshape(params[W1_start:W1_end], (self.feature_no , self.hidden_layer_neurons))
+        W2_end = W1_end + self.hidden_layer_neurons*self.output_layer_size
+        self.w2 = np.reshape(params[W1_end:W2_end], (self.hidden_layer_neurons, self.output_layer_size))
+    
